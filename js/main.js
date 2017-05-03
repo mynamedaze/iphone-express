0f5c0709c585b9d846735bed30d95b1c7fbfe06d
Murat Kudainetov <murat.kudainetov@gmail.com>
2017-05-03 17:55:04 +0300

    доделал шапку, 80% интро. Пока не до конца оформлена форма отправки и не прикреплен php файл

--- js/main.js
+++ js/main.js
@@ -2,3 +2,23 @@ var formatted = new Formatter(document.getElementById('telephone-field'), {
   'pattern': '+7 ({{999}}) {{999}}-{{9999}}',
   'persistent': true
 });
+
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    responsive:{
+        0:{
+            items:1
+        },
+        768:{
+            items:3,
+            touchDrag: false,
+            mouseDrag: false
+        },
+        1362:{
+            items:3,
+            touchDrag: false,
+            mouseDrag: false
+        }
+    }
+  });
+});
